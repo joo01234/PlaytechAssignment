e46d20aaea53e8309e2c9fb4d32f3685c288a95a
Karl Joosep Kahk <kahkkarljoosep@gmail.com>
2023-03-22 14:12:21 +0200

    input checker

--- src/main/java/InputChecker.java
+++ src/main/java/InputChecker.java
@@ -9,10 +9,20 @@ public class InputChecker {
         if (lineArr.length != 6) {
             return false;
         }
+
         for (int i = 0; i < 6; i++) {
             if (i < 3) {
                 if (!isNumber(lineArr[i])) {
-                    System.out.println(i);
+                    return false;
+                }
+            }
+            else if (i == 3) {
+                if (!isCorrectActionFormat(lineArr[i])) {
+                    return false;
+                }
+            }
+            else {
+                if (!isCorrectHandFormat(lineArr[i])) {
                     return false;
                 }
             }
@@ -22,6 +32,24 @@ public class InputChecker {
 
     public static boolean isNumber(String elem) {
         Pattern pattern = Pattern.compile("^[0-9]+$");
+        Matcher matcher = pattern.matcher(elem);
+        if (!matcher.matches()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isCorrectActionFormat(String elem) {
+        Pattern pattern = Pattern.compile("^[PD] [A-Za-z]+$");
+        Matcher matcher = pattern.matcher(elem);
+        if (!matcher.matches()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isCorrectHandFormat(String elem) {
+        Pattern pattern = Pattern.compile("^-|(?:(?:(?:[2-9JQKAjqka]|10)[HDSChdsc]-)+(?:[2-9JQKAjqka]|10)[HDSChdsc])$");
         Matcher matcher = pattern.matcher(elem);
         if (!matcher.matches()) {
             return false;
