4f51af8f5223a1909bd720ab894cffa4fec686be
Karl Joosep Kahk <kahkkarljoosep@gmail.com>
2023-03-22 09:42:16 +0200

    t3

--- src/main/java/Analyser.java
+++ src/main/java/Analyser.java
@@ -6,6 +6,6 @@ public class Analyser {
     public static void main(String[] args) {
         Reader r = new Reader();
         r.readFile("src/main/resources/game_data.txt");
-        System.out.println(r.readLine());
+        System.out.println(r.readLine() + "t");
     }
 }
